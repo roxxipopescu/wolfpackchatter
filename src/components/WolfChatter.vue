691fdf90bc3800b8fba70c873ba41c3b429927c2
Popescu <rpopescu@she.local>
2019-07-01 18:25:01 +0300

    add one single marker onClick()

--- src/components/WolfChatter.vue
+++ src/components/WolfChatter.vue
@@ -1,15 +1,26 @@
 <template>
   <div id="map">
+    <div class="choose-location">
+      {{chooseLocation}}
+    </div>
     <div class="chat-panel">
       <div class="chat-init">
-        <input type="text" class="name-inputfield form-control" v-model="username" placeholder="Your name"/>
+        <input type="text" class="name-inputfield form-control" v-model="username" :placeholder="yourNamePlaceholder"/>
         <button class="start-chat btn btn-dark" type="button" v-on:click="openChat()">Start chatting</button>
       </div>
       <div class="open-chat">
         <img class="user-picture" src="../../static/images/user-picture.png"/>
         <span class="username">{{username}}</span>
         <div class="chats-container">
+          <i class="fa fa-search"></i>
           <input type="text" class="search-inputfield form-control" v-model="search" placeholder="Chat name"/>
+          <i class="fa fa-plus-circle" v-on:click="addNewChat"></i>
+          <div class="chatlist">
+            <div class="empty-chatlist">
+              <p>{{emptyChatsMsg}}</p>
+              <p>{{plusButtonInfo}}</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -18,32 +29,70 @@
 
 <script>
 import "leaflet/dist/leaflet.css"
+import "font-awesome/css/font-awesome.css";
 import L from 'leaflet';
+import $ from 'jquery';
 
 export default {
 
   data: function() {
     return {
       username: "",
-      search: ""
+      search: "",
+      emptyChatsMsg: "Nothing to show here.",
+      plusButtonInfo: "Use the + button to create a new chat",
+      yourNamePlaceholder: "Your name",
+      chooseLocation: "Choose the chat location on the map",
+      map: {},
+      myIcon: {},
+      marker: {}
     };
   },
   mounted: function(){
+
+    var tiles = new L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png');
     // Initialize the map and assign it to a variable for later use
-    var map = L.map('map', {
+    this.map = L.map('map', {
         // Set latitude and longitude of the map center (required)
         center: [46.7712, 23.6236],
         // Set the initial zoom level, values 0-18, where 0 is most zoomed-out (required)
-        zoom: 5
+        zoom: 5,
+        layers: [tiles]
     });
 
     // Create a Tile Layer and add it to the map
-    var tiles = new L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png').addTo(map);
+   
+
+    this.myIcon = L.icon({
+        iconUrl: 'https://unpkg.com/leaflet@1.1.0/dist/images/marker-icon.png',
+        iconRetinaUrl: 'https://unpkg.com/leaflet@1.1.0/dist/images/marker-icon-2x.png',
+        shadowUrl: 'https://unpkg.com/leaflet@1.1.0/dist/images/marker-shadow.png',
+        iconSize:    [25, 41],
+        iconAnchor:  [12, 41],
+        popupAnchor: [1, -34],
+        tooltipAnchor: [16, -28],
+        shadowSize:  [41, 41]
+    });
+    
   },
+  
   methods:{
+    addMarker(e){     
+      if(this.marker){
+        this.map.removeLayer(this.marker);
+      }
+      this.marker = L.marker(e.latlng, {icon: this.myIcon}).addTo(this.map);        
+    },
     openChat(){
       $('.chat-init').hide();
       $('.open-chat').show();
+    },
+    addNewChat(event){
+      $('.choose-location').show();
+      
+      
+        this.map.on('click', this.addMarker);
+      
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import App from './App.vue'
 import store from './store'
 import 'bootstrap'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import jQuery from 'jquery';
-window.$ = jQuery;
 
 Vue.config.productionTip = false
 
